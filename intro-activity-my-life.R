c59bbd4c4b7d3e5f956b95d31966e96004f2099d
Jenine Harris <harrisj@wustl.edu>
2021-08-25 12:23:22 -0500

    Add files via upload

--- intro-activity-my-life.R
+++ intro-activity-my-life.R
@@ -1,80 +1,59 @@
-###########################################
-# Project title: My life in months
-# Purpose: Plot where I've lived by the month
-# Author: Jenine 
-# Edit date: August 27, 2020
+################PROLOG#####################
+# Project title: 
+# Purpose: 
+# Author:  
+# Edit date: 
 # Data: No external data files used
 ###########################################
 
+
 # STEP 1: TYPE IN DATA
 
-# the eras of my life (choose 10 or fewer)
-# CHANGE THIS CODE TO YOUR ERAS
-era <- c("Eugene, Oregon", 
-         "Corvallis, Oregon", 
-         "Eugene, Oregon 2",
-         "Avignon, France", 
-         "Eugene, Oregon 3",
-         "Washington DC", 
-         "Eugene, Oregon 4", 
-         "Bend, Oregon", 
-         "St. Louis, Missouri")
+# the eras of my life (8 or fewer)
+era <- c()
 
-# the months I spent in each era (same number and order as eras)
-# CHANGE THIS CODE TO YOUR MONTHS
-months <- c(213, 
-            21, 
-            18,
-            4, 
-            12, 
-            16, 
-            67, 
-            12, 
-            245) 
+# the number of months I spent in each era (same number and order as eras)
+months <- c()
 
-# STEP 2: MAKE THE GRAPH
 
-##############################
-# GRAPH OPTION 1: THE WAFFLE (some operating systems don't like this package)
-# open the waffle package (install first through Tools menu)
-library(package = "waffle")
+# STEP 2: COMBINE THE DATA INTO A DATA FRAME
 
-# the waffle plot requires the data to be in a table
-# combine the vectors into a table
-my_life <- as.table(setNames(object = months, nm = era))
-my_life 
+# combine the vectors into a data frame
+my_life_df <- data.frame(era, months)
 
-# make a waffle plot with each column showing 1 year
-# MAKE SURE THE NUMBER OF COLORS IS THE SAME AS THE NUMBER OF 
-# ERAS
-# (OPTIONAL) CHANGE COLORS USING COLORBREWER2.ORG
-waffle(parts = my_life, rows = 12, 
-       colors = c("#a6cee3",
-                  "#1f78b4",
-                  "#b2df8a",
-                  "#33a02c",
-                  "#fb9a99",
-                  "#e31a1c",
-                  "#fdbf6f",
-                  "#ff7f00",
-                  "#cab2d6"))
+# explain to R that the era data should stay in order
+my_life_df$era <- factor(my_life_df$era, levels = my_life_df$era)
 
-###############################
-# GRAPH OPTION 2: THE BAR GRAPH 
+
+# STEP 3: MAKE THE GRAPH
+
+
+# Graph option 1: A bar graph
 # open the tidyverse package (install first through Tools menu)
 library(package = "tidyverse")
 
-# the bar plot needs a data frame rather than a table
-# combine the vectors into a data frame
-my_life_df <- data.frame(era, months)
-my_life_df$era <- factor(my_life_df$era, levels = my_life_df$era)
-
 # make the plot
 my_life_df %>% 
-  ggplot(aes(x = era, y = months, fill = era)) +
-  geom_col() +
-  coord_flip() +
-  theme_minimal() 
+  ggplot(aes(x = era, y = months)) +
+  geom_col() 
 
-# WHEN FINISHED EDITING, HIGHLIGHT THE CODE AND CLICK "RUN"
+
+# Graph option 2: A waffle plot
+# there is a package for making waffle plots
+# some combos of R Studio & OS don't like it
+
+# open waffle package (install first through Tools menu)
+library(package = "waffle")
+
+# make a waffle plot with each column showing 1 year
+waffle(parts = my_life_df, rows = 12)
+
+
+# Resources for doing more with your waffle and bar--
+# Waffle: https://github.com/hrbrmstr/waffle
+# Bar: https://ggplot2.tidyverse.org/reference/geom_bar.html
+# Colors: https://colorbrewer2.org/
+  
+  
+
 
